928b9b35a91db08312542d3c949c45d3713502d8
Adam Lipert <a.lipert@protonmail.com>
2022-01-18 10:29:40 +0100

    Add animation on score

--- scripts/ingame.js
+++ scripts/ingame.js
@@ -1,5 +1,5 @@
 var socket = io();
-var scores = document.getElementById("table");
+var scores_global;
 var localstorage = window.localStorage;
 
 window.onload = function () {
@@ -11,31 +11,71 @@ window.onload = function () {
     localstorage.getItem("teamB_score");
 };
 
-socket.on("teams", function (names, scores) {
+socket.on("teams", async function (names, scores) {
+  scores_global = scores;
   document.getElementById("teamA").innerHTML = names.teamA;
   document.getElementById("teamB").innerHTML = names.teamB;
 
-  document.getElementById("teamA_score").innerHTML = scores.teamA;
-  document.getElementById("teamB_score").innerHTML = scores.teamB;
+  var lastScoreA = document.getElementById("teamA_score").innerHTML;
+  var lastScoreB = document.getElementById("teamB_score").innerHTML;
+  if (lastScoreA != scores.teamA) {
+    var change = scores.teamA - lastScoreA;
+    animate("A", change);
+  } else if (lastScoreB != scores.teamB) {
+    var change = parseInt(scores.teamB) - parseInt(lastScoreB);
+    animate("B", change);
+  }
 
   localstorage.setItem("teamA", names.teamA);
   localstorage.setItem("teamB", names.teamB);
   localstorage.setItem("teamA_score", scores.teamA);
   localstorage.setItem("teamB_score", scores.teamB);
-
-  animate();
 });
-
-function animate() {
-  anime({
-    targets: document.getElementById("teamA_score"),
-    translateX: 0,
-    rotate: "1turn",
+function animate(team, change) {
+  if (team === "A") {
+    var overlay = document.getElementById("overlayA");
+  } else if (team === "B") {
+    var overlay = document.getElementById("overlayB");
+  }
+  var tl = anime.timeline({
+    targets: overlay,
+    easing: "easeInOutExpo",
+    duration: 800,
+  });
+  tl.add({
+    innerHTML: change,
+    round: 1,
+    duration: 100,
+  })
+  tl.add({
+    height: "106px",
+    opacity: 1,
+  });
+  if (team === "A") {
+    tl.add({
+      targets: "#teamA_score",
+      round: 1,
+      innerHTML: scores_global.teamA,
+      duration: 100,
+    });
+  } else {
+    tl.add({
+      targets: "#teamB_score",
+      round: 1,
+      innerHTML: scores_global.teamB,
+      duration: 100,
+    });
+  }
+  tl.add({
+    height: 0,
+    opacity: 0,
     duration: 800,
   });
 }
 
 socket.on("clear", () => {
   localstorage.clear();
+  localstorage.setItem("teamA_score", 0);
+  localstorage.setItem("teamB_score", 0);
   location.reload();
 });
